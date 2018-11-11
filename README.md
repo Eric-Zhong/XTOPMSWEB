1bf9ab35b62fa2da241f737792b470f1a0b7fd73
Eric <xu.zhong@hotmail.com>
2018-11-11 17:31:46 +0800

    Initiate web site code from ant design pro 2.1.1.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # XTOPMSWEB
 XTO Project Management Web Project.
+Web UI based at ant design pro 2.1.1.
+API based at ABP asp.net core.
---
+++ src/pages/.umi/initDva.js
@@ -0,0 +1,82 @@
+import dva from "dva";
+import createLoading from "dva-loading";
+
+const runtimeDva = window.g_plugins.mergeConfig("dva");
+let app = dva({
+  history: window.g_history,
+
+  ...(runtimeDva.config || {})
+});
+
+window.g_app = app;
+app.use(createLoading());
+(runtimeDva.plugins || []).forEach(plugin => {
+  app.use(plugin);
+});
+
+app.model({
+  namespace: "global",
+  ...require("/Work/Dotnet/XTOPMSWEB/src/models/global.js").default
+});
+app.model({
+  namespace: "list",
+  ...require("/Work/Dotnet/XTOPMSWEB/src/models/list.js").default
+});
+app.model({
+  namespace: "login",
+  ...require("/Work/Dotnet/XTOPMSWEB/src/models/login.js").default
+});
+app.model({
+  namespace: "project",
+  ...require("/Work/Dotnet/XTOPMSWEB/src/models/project.js").default
+});
+app.model({
+  namespace: "setting",
+  ...require("/Work/Dotnet/XTOPMSWEB/src/models/setting.js").default
+});
+app.model({
+  namespace: "user",
+  ...require("/Work/Dotnet/XTOPMSWEB/src/models/user.js").default
+});
+app.model({
+  namespace: "register",
+  ...require("/Work/Dotnet/XTOPMSWEB/src/pages/User/models/register.js").default
+});
+app.model({
+  namespace: "activities",
+  ...require("/Work/Dotnet/XTOPMSWEB/src/pages/Dashboard/models/activities.js")
+    .default
+});
+app.model({
+  namespace: "chart",
+  ...require("/Work/Dotnet/XTOPMSWEB/src/pages/Dashboard/models/chart.js")
+    .default
+});
+app.model({
+  namespace: "monitor",
+  ...require("/Work/Dotnet/XTOPMSWEB/src/pages/Dashboard/models/monitor.js")
+    .default
+});
+app.model({
+  namespace: "form",
+  ...require("/Work/Dotnet/XTOPMSWEB/src/pages/Forms/models/form.js").default
+});
+app.model({
+  namespace: "rule",
+  ...require("/Work/Dotnet/XTOPMSWEB/src/pages/List/models/rule.js").default
+});
+app.model({
+  namespace: "profile",
+  ...require("/Work/Dotnet/XTOPMSWEB/src/pages/Profile/models/profile.js")
+    .default
+});
+app.model({
+  namespace: "error",
+  ...require("/Work/Dotnet/XTOPMSWEB/src/pages/Exception/models/error.js")
+    .default
+});
+app.model({
+  namespace: "geographic",
+  ...require("/Work/Dotnet/XTOPMSWEB/src/pages/Account/Settings/models/geographic.js")
+    .default
+});
